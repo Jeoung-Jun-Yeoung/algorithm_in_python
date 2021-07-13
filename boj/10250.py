cd10ebf1572ca43860fcb304d574a418d796ee57
Jeoung-Jun-Yeoung <dbahsmssk@naver.com>
2021-07-13 15:24:20 +0900

    solved 10250.py

--- boj/10250.py
+++ boj/10250.py
@@ -3,11 +3,16 @@ n = int(input())
 for _ in range(0,n):
     h,w,o = map(int,input().split())
 
-    y = o%h
-    x = (o//h) + 1
+    if o%h == 0:
+        y = h
+        x = (o//h) 
+    else:
+        y = o%h
+        x = (o//h) + 1
     
     if x < 10:
         anw = "0"
         anw = anw + str(x)
-        
-    print(str(y)+anw)
+        print(str(y)+anw)
+    else:
+        print(str(y)+str(x))
