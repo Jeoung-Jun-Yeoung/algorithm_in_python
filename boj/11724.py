755a68e1ce6ebdb0db3f9764082e064f38d99a58
junyeoung <dbahsmssk@naver.com>
2021-08-17 22:04:26 +0900

    git solved

--- boj/11724.py
+++ boj/11724.py
@@ -1,51 +1,20 @@
 from sys import stdin
-<<<<<<< HEAD
-from collections import defaultdict, deque
-import sys
-sys.setrecursionlimit(10000)
-
-
-def dfs(node):
-    visted[node] = 1
-    for i in graph[node]:
-        if not visted[i]:
-            dfs(i)
-
+from collections import deque, defaultdict
 
 n, m = map(int, stdin.readline().split())
 
-graph = [[] for _ in range(n + 1)]
-visted = [[False] for _ in range(n + 1)]
-
-for i in range(m):
-    x, y = list(map(int, stdin.readline().split()))
-    graph[x].append(y)
-    graph[y].append(x)
-
-ck = 0
-
-
-for i in range(n):
-    if not visted[i]:
-        dfs(i)
-        ck += 1
-print(ck)
-=======
-from collections import deque,defaultdict
-
-n,m = map(int,stdin.readline().split())
-
 graph = defaultdict(list)
+
 
 def dfs(start):
     visted[start] = True
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         if visted[start] != False and graph[start][i] == 1:
             dfs(i)
 
 
 for i in range(m):
-    n1,n2 = map(int,stdin.readline().split())
+    n1, n2 = map(int, stdin.readline().split())
     graph[n1].append(n2)
     graph[n2].append(n1)
 
@@ -53,7 +22,7 @@ visted = [False] * (n + 1)
 
 ck = 0
 
-for i in range(1,n+1):
+for i in range(1, n+1):
     if not visted[i]:
         dfs(i)
         print(visted)
@@ -61,6 +30,3 @@ for i in range(1,n+1):
     print(ck)
 
 print(ck)
-
-
->>>>>>> 5b4d40da5e2abad2a911f80dad78ebfb3928c1ba
