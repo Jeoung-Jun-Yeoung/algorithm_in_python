70b2fa3bdf9a63823ae1c9259db0c9145c7b888c
Jeoung-Jun-Yeoung <dbahsmssk@naver.com>
2022-02-18 15:04:09 +0900

    solvin boj 11650

--- boj/11650.py
+++ boj/11650.py
@@ -8,25 +8,20 @@ for i in range(N):
     arrx.append(x)
     arry.append(y)
 
-for cur in range(0, N):  # 01234
+for cur in range(N):  # 01234
     for next in range(1, N - 1):  # 123
-        print()
-        print(arrx[cur], arrx[next])
-        print(arry[cur], arry[next])
-        # if(arrx[next] < arrx[cur]):
-        #     tempx = arrx[cur]
-        #     arrx[cur] = arrx[next]
-        #     arrx[next] = tempx
-        #     tempy = arry[cur]
-        #     arry[cur] = arry[next]
-        #     arry[next] = tempy
-        # if(arrx[next] == arrx[cur] and arry[next] < arry[cur]):
-        #     tempx = arrx[cur]
-        #     arrx[cur] = arrx[next]
-        #     arrx[next] = tempx
-        #     tempy = arry[cur]
-        #     arry[cur] = arry[next]
-        #     arry[next] = tempy
+        # print()
+        # print(arrx[cur], arrx[next])
+        # print(arry[cur], arry[next])
+        if(arrx[cur] > arrx[next]):
+            tempx = arrx[cur]
+            arrx[cur] = arrx[next]
+            arrx[next] = tempx
+            tempy = arry[cur]
+            arry[cur] = arry[next]
+            arry[next] = tempy
+        if(arrx[cur] == arrx[next] and arry[cur] > arry[next]):
+            arry[cur] = arry[next]
+            arry[next] = tempy
 
-
-#print(arrx, arry)
+print(arrx, arry)
