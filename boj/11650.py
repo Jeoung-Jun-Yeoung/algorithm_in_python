ed4b6bb41c5cfdd36e358161a942ecf4b848acfa
Jeoung-Jun-Yeoung <dbahsmssk@naver.com>
2022-02-20 23:44:05 +0900

    solve boj 11650

--- boj/11650.py
+++ boj/11650.py
@@ -1,27 +1,11 @@
-dic = {}
 
 N = int(input())
-arrx = list()
-arry = list()
+data = []
+
 for i in range(N):
-    x, y = map(int, input().split())
-    arrx.append(x)
-    arry.append(y)
+    data.append(list(map(int, input().split())))
 
-for cur in range(N):  # 01234
-    for next in range(1, N - 1):  # 123
-        # print()
-        # print(arrx[cur], arrx[next])
-        # print(arry[cur], arry[next])
-        if(arrx[cur] > arrx[next]):
-            tempx = arrx[cur]
-            arrx[cur] = arrx[next]
-            arrx[next] = tempx
-            tempy = arry[cur]
-            arry[cur] = arry[next]
-            arry[next] = tempy
-        if(arrx[cur] == arrx[next] and arry[cur] > arry[next]):
-            arry[cur] = arry[next]
-            arry[next] = tempy
+data.sort(key=lambda x: (x[0], x[1]))
 
-print(arrx, arry)
+for i in data:
+    print(i[0], i[1])
