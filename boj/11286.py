20fd20ee4defdd5355b393f4bcbc490ff7889610
Jeoung-Jun-Yeoung <dbahsmssk@naver.com>
2022-07-21 14:24:21 +0900

    adds: codes updeate

--- boj/11286.py
+++ boj/11286.py
@@ -6,6 +6,10 @@ N = int(input())
 while N > 0:
     x = int(input())
     if x == 0:
-        print(heapq.heappop(pq))
+        if len(pq) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(pq))
     else:
         heapq.heappush(pq, x)
+    N = N - 1
