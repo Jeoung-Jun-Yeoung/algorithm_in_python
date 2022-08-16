d607f5070b29ac0b789ed96a42f38363565a72c2
Jeoung-Jun-Yeoung <dbahsmssk@naver.com>
2022-08-16 16:12:00 +0900

    2512 ing~

--- boj/2512.py
+++ boj/2512.py
@@ -11,14 +11,23 @@ total = int(input())
 
 if(sum(budget) <= total):
     print(max(budget))
+else:
+    before_mid = 0
+    mid = total
+    while mid >= min(budget):
+        before_mid = mid
+        mid = mid // 2
 
+    index = len(budget) // 2
 
-print(min(budget))
-before_mid = 0
-mid = total
-while mid >= min(budget):
-    before_mid = mid
-    mid = mid // 2
+    while before_mid <= budget[index]:
+        index = index // 2
+    sum = 0
+    for i in range(0, index + 1):
+        sum += before_mid - budget[i]
 
-index = len(budget) / 2
-while before_mid >= budget(index):
+    sum = sum // (index + 1)
+
+    before_mid = before_mid + sum
+
+    print(before_mid)
