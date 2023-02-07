610e0b8c3af625141e6140e346cc6309c6d3f372
Jeoung-Jun-Yeoung <dbahsmssk@naver.com>
2023-02-07 10:49:00 +0900

    solve.py

--- 과외/숙제/2563.py
+++ 과외/숙제/2563.py
@@ -8,4 +8,19 @@ n = int(input())
 
 
 # 100 * 100 도화지를 그리고, 채운다. 이후 채운 개수를 센다.
-paper = [[0] for i in range(100)]
+paper = [[0] * 100 for i in range(100)]
+
+for i in range(n):
+    x, y = map(int, input().split())
+
+    for i in range(x, x+10, 1):
+        for j in range(y, y+10, 1):
+            if paper[i][j] == 0:
+                paper[i][j] = 1
+ck = 0
+for i in range(100):
+    for j in range(100):
+        if paper[i][j] == 1:
+            ck += 1
+
+print(ck)
