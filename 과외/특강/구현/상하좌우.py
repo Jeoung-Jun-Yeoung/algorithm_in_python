385e5033cb1d52b0b675316caad1a87be4df2df4
Jeoung-Jun-Yeoung <dbahsmssk@naver.com>
2023-02-08 15:38:19 +0900

    add : 이코테 상하좌우

--- 과외/특강/구현/상하좌우.py
+++ 과외/특강/구현/상하좌우.py
@@ -2,14 +2,51 @@ N = int(input())
 
 board = [[0] * N for _ in range(N)]
 
+diry = (0, 0, 1, -1)  # 행 가로
+dirx = (1, -1, 0, 0)  # 열 세로
+
+# 0 : 오른쪽
+# 1 : 왼쪽
+# 2 : 남쪽
+# 3 : 북쪽
+
 
 def is_valid_board(x, y, n):
-    return (x >= 0 and x < n) or (y >= 0 and y < n)
+    return (0 < x < n) and (0 < y < n)
 
 
 movePlan = list(input().split())
-
+# 시작좌표 (0,0)
 print(board)
 print(movePlan)
-
-# +1을 해주자
+x = 1
+y = 1
+for dir in movePlan:
+    dx = 0
+    dy = 0
+    if dir == 'R':
+        dy = y + diry[0]
+        dx = x + dirx[0]
+        if is_valid_board(dy, dx, N):
+            y = dy
+            x = dx
+    elif dir == 'L':
+        dy = y + diry[1]
+        dx = x + dirx[1]
+        if is_valid_board(dy, dx, N):
+            y = dy
+            x = dx
+    elif dir == 'D':
+        dy = y + diry[2]
+        dx = x + dirx[2]
+        if is_valid_board(dy, dx, N):
+            y = dy
+            x = dx
+    elif dir == 'U':
+        dy = y + diry[3]
+        dx = x + dirx[3]
+        if is_valid_board(dy, dx, N):
+            y = dy
+            x = dx
+    print(f'Y {y} X {x} dir {dir}')
+print(f'y {y} x {x} ')
