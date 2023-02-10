11690da9a4558487e8640f363aff27b75cc7e8e9
Jeoung-Jun-Yeoung <dbahsmssk@naver.com>
2023-02-10 09:39:30 +0900

    solve boj

--- boj/18320.py
+++ boj/18320.py
@@ -2,4 +2,18 @@ n = int(input())
 
 home = list(map(int, input().split()))
 home.sort()
-print(home)
+print(home[(n-1) // 2])
+
+# nowDis = 300000
+# temp = 0
+# minDis = []
+# for i in range(4):
+#     distance = 0
+#     for j in range(4):
+#         distance += abs(home[i] - home[j])
+#     minDis.append(distance)
+
+# val = min(minDis)
+# print(home[minDis.index(val)])
+
+# print(index.find(min(index)))
